814a885608041dcdf620b159f807c23f7c3ead1b
softlolx <softlolx@gmail.com>
2023-06-06 22:59:06 +0300

    fix proxy

--- src/store/mainSlice/getStoreValue.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { getStore } from './getStore';
-// import { CounterSchema } from '../types/counterSchema';
-
-
-export const getStoreValue = createSelector(getStore, (store: any) => store.value);
